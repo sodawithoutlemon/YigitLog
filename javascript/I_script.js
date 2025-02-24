23ddefbf8fb0d06978dc7d7e0df2b5922943d104
sodawithoutlemon <ytyigit2021@hotmail.com>
2025-02-25 00:24:52 +0300

    Resume

--- javascript/I_script.js
+++ javascript/I_script.js
@@ -9,11 +9,10 @@ $('.img').css('background-image','url('+image+')');
 
 if (window.innerWidth <= 720) {
     
-    $(".phone-first").html("<a href='https://github.com/sodawithoutlemon'><div class='butdivmiddle'><button type='button' class='btn btn-outline-light btn-lg third'><i class='bi bi-github'></i><br><h5>GitHub</h5></button></div></a><a href='https://www.instagram.com/grindwithyigit/'><div class='butdivmiddle'><button type='button' class='btn btn-outline-light btn-lg fourth'><i class='bi bi-instagram'></i><br><h5 class='instagram'>Instagram</h5></button></div></a>")
-
-    $(".phone-second").html("<a href='data.html?topic=anime-alphabetical'><div class='butdivside'><button type='button' class='btn btn-outline-light btn-lg first'><h5>Anime</h5></button></div></a><a href='data.html?topic=book-alphabetical'><div class='butdivside'><button type='button' class='btn btn-outline-light btn-lg second'><h5>Book</h5></button></div></a>")
+    $(".phone-first").html('<a href="https://www.instagram.com/tcyigitozdemir/"><div class="butdivside"><button type="button" class="btn btn-outline-light btn-lg fifth"><i class="bi bi-instagram"></i><br><h5>Instagram</h5></button></div></a><a href="mailto:yyigitoozdemir@gmail.com"><div class="butdivside"><button type="button" class="btn btn-outline-light btn-lg sixth"><i class="bi bi-send-fill"></i><br><h5>Contact Me</h5></button></div></a>')
+    $(".phone-second").html('<a class="git" href="https://apps.apple.com/us/developer/yigit-ozdemir/id1790956332"><div class="butdivmiddle"><button type="button" class="btn btn-outline-light btn-lg third"><i class="bi bi-apple"></i><br><h5>A&Y Creations</h5></button></div></a><a class="inst" href="https://play.google.com/store/apps/dev?id=8654064472652463037"><div class="butdivmiddle"><button type="button" class="btn btn-outline-light btn-lg fourth"><i class="bi bi-google-play"></i><br><h5 class="instagram">A&Y Creations</h5></button></div></a>')
     $(".phone-middle").html("<div class='midinfo'><h1>YigitLog</h1></div>")
-    $(".phone-fourth").html("<a href='data.html?topic=movie-alphabetical'><div class='butdivside'><button type='button' class='btn btn-outline-light btn-lg fifth'><h5>Movie</h5></button></div></a><a href='data.html?topic=game-alphabetical'><div class='butdivside'><button type='button' class='btn btn-outline-light btn-lg sixth'><h5>Game</h5></button></div></a>")
+    $(".phone-fourth").html('<a href="https://github.com/sodawithoutlemon"><div class="butdivmiddle"><button type="button" class="btn btn-outline-light btn-lg seventh"><i class="bi bi-github"></i><br><h5>GitHub</h5></button></div></a><a href="https://www.instagram.com/tcyigitozdemir/"><div class="butdivmiddle"><button type="button" class="btn btn-outline-light btn-lg eighth lucky"><i class="bi bi-linkedin"></i><br><h5>LinkedIn</h5></button></div></a>')
 
 }
 
